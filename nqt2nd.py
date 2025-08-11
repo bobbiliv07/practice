8c6a4bd88f3c2b3c084889009d748546cf61d7a6
bobbiliv07 <vbobbili@student.gitam.edu>
2025-08-11 10:15:13 +0530

    finding the longest sequence in increasing

---
+++ nqt2nd.py
@@ -0,0 +1,23 @@
+def finding_largest(array):
+    if not array:
+        return []
+    current_sum=array[0]
+    current_sub=[array[0]]
+    highest_sum=array[0]
+    highest_sub=[array[0]]
+    for i in range(1,len(array)):
+        if array[i]>array[i-1]:
+            current_sum+=array[i]
+            current_sub.append(array[i])
+        else:
+            if current_sum > highest_sum:
+                highest_sum=current_sum
+                highest_sub=current_sub
+            current_sum=array[0]
+            current_sub=[array[0]]
+        if current_sum>highest_sum:
+            highest_sub=current_sub
+    return highest_sub
+input_list = [1,2,3,5,1,2,3]
+result = finding_largest(input_list)
+print(sum(result))
