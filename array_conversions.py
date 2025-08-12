7bf3fb5ea1e9f63d9374dd0a54e64c44929058d5
bobbiliv07 <vbobbili@student.gitam.edu>
2025-08-12 20:26:26 +0530

    added extra code for user interaction

--- array_conversions.py
+++ array_conversions.py
@@ -2,7 +2,6 @@ def array_to_number(array):
     n=len(array)-1
     number=0
     for i in array:
-        print(i)
         number=number+i*10**n
         n-=1
     return number
@@ -14,7 +13,17 @@ def number_to_array(number):
         digits.insert(0,digit)
         number=number//10
     return digits
-x=124
+option=int(input("enter your choice  1 for num-array 2 for array-num :"))
+match option :
+    case 1:
+        number=int(input("enter your number"))
+        print(number_to_array(number))
+    case 2:
+        array=[int(element) for element in input("elements by space: ").split()]
+
+        print(array_to_number(array))
+    case _:
+        print('not a valid option')
 
 
 
